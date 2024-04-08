6c7d404d52a6dd37b0d0bd760eb648a0250893ea
Cindy Nguyen <cnguyen5138@gmail.com>
2024-04-08 12:14:52 -0400

    debug done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,14 +1,27 @@
 const loop0UpTo10 = () => {
+  for (let i = 0; i < 10; i++) {
+    console.log(i)
+  }
 };
 
 const loop5to10 = () => {
+  for (let i = 5; i <= 10; i++) {
+    console.log(i)
+  }
 };
+
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for (let i = 0; i < 10; i += 2) {
+    console.log(i)
+  }
 };
 
 const countdown5to0 = () => {
+  for (let i = 5; i >= 0; i--) {
+    console.log(i)
+  }
 };
 
 const loopUpToNum = () => {
